9fccf99c754c8c43772725eec1e4d02ab527639c
Brian Kim <bryo.kim1@gmail.com>
2024-03-31 13:11:39 +0300

    refactor: create HeroSection component to hold hero content

---
+++ components/HeroSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="lg:w-2/3 mx-auto p-3 mb-10 sm:mb-20 flex flex-col items-center justify-center backdrop-blur-sm rounded-lg"
+  >
+    <div class="mb-3 text-center text-base sm:text-xl">
+      Simple frontend for the
+      <ULink
+        to="https://github.com/bryokim/bible-api"
+        target="_blank"
+        active-class="text-primary"
+        inactive-class="text-sky-500 hover:text-emerald-500"
+        >Bible-API
+        <UIcon name="i-heroicons-arrow-top-right-on-square-16-solid" />
+      </ULink>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,5 @@
 <template>
-  <div
-    class="lg:w-2/3 mx-auto h-20 mb-10 sm:mb-20 flex items-center justify-center backdrop-blur-sm-lg drop-shadow-md shadow-md bg-gray-900/10 dark:bg-white/5 rounded-lg"
-  >
-    <p class="text-center">
-      Welcome to Bible-UI. Frontend for the
-      <ULink
-        to="https://github.com/bryokim/bible-api"
-        target="_blank"
-        active-class="text-primary"
-        inactive-class="text-sky-500 hover:text-emerald-500"
-        >Bible-API
-      </ULink>
-    </p>
-  </div>
+  <HeroSection />
 
   <div class="lg:w-2/3 mx-auto">
     <RandomVerseSection />
