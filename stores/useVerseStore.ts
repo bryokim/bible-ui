3ae595608874ae3d4b74999d7e768d9b3c86c404
Brian Kim <bryo.kim1@gmail.com>
2024-04-05 12:52:18 +0300

    refactor: move the fetching to a new function to avoid duplication

--- stores/useVerseStore.ts
+++ stores/useVerseStore.ts
@@ -35,7 +35,7 @@ export const useVerseStore = defineStore(
         book_group?: string;
         bible_version?: string;
       }
-    ) {
+    ): Promise<VerseResponseType | void> {
       const key: string = `${queryParams.book || "Genesis"}_${
         queryParams.chapter || 1
       }_${queryParams.verse?.replace("-", "_") || "1"}_${
@@ -50,33 +50,13 @@ export const useVerseStore = defineStore(
         throw new HTTPError(value);
       }
 
-      return await $fetch(endpoint, {
-        method: "GET",
-        baseURL: baseURL,
-        query: {
-          book: queryParams.book || "Genesis",
-          chapter: queryParams.chapter || 1,
-          verse: queryParams.verse || "1",
-          book_group: queryParams.book_group,
-          bible_version: queryParams.bible_version,
-        },
-      })
-        .then((data) => {
-          verses.value[key] = data;
-          return verses.value[key];
-        })
-        .catch((e: any) => {
-          // Save only errors that are caused by client
-          if (e.status.toString().startsWith("4")) {
-            verses.value[key] = {
-              error: true,
-              message: e.message,
-              data: e.data,
-              status: e.status.toString(),
-            };
-          }
-          throw new HTTPError(e);
-        });
+      return await _fetch(key, endpoint, baseURL, {
+        book: queryParams.book || "Genesis",
+        chapter: queryParams.chapter || 1,
+        verse: queryParams.verse || "1",
+        book_group: queryParams.book_group,
+        bible_version: queryParams.bible_version,
+      });
     }
 
     /**
@@ -96,7 +76,7 @@ export const useVerseStore = defineStore(
       queryParams: {
         bible_version?: string;
       }
-    ) {
+    ): Promise<VerseResponseType | void> {
       const key: string = `DAILY_VERSE_${queryParams.bible_version || "NIV"}`;
 
       if (verses.value[key]) {
@@ -116,12 +96,33 @@ export const useVerseStore = defineStore(
         if (value.error) throw new HTTPError(value);
       }
 
+      return await _fetch(key, endpoint, baseURL, {
+        bible_version: queryParams.bible_version,
+      });
+    }
+
+    /**
+     * Fetches verses from the given endpoints.
+     *
+     * @see HTTPError
+     * @throws HTTPError
+     *
+     * @param key the key to store the response in in the verses value
+     * @param endpoint the api endpoint to request from
+     * @param baseURL the api's base URL
+     * @param queryParams query parameters passed to the request
+     * @returns the fetched verse
+     */
+    async function _fetch(
+      key: string,
+      endpoint: string,
+      baseURL: string,
+      queryParams: object
+    ): Promise<VerseResponseType | void> {
       return await $fetch(endpoint, {
         method: "GET",
         baseURL: baseURL,
-        query: {
-          bible_version: queryParams.bible_version,
-        },
+        query: queryParams,
       })
         .then((data) => {
           verses.value[key] = data;
